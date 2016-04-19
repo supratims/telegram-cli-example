b3b6b2036a27ffea3bd142c3ed976551b5b0ee96
supratim <supratims@gmail.com>
2016-04-20 00:00:14 +0100

    initial commit

---
+++ cli.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * Get started here https://core.telegram.org/bots/api
+ * Create a new bot by using the @botfather bot using the telegram app , and copy your token here
+ * You can use webhook or use this cli script to get latest messages
+ * @todo send replies to messages using the bot
+ */
+define('BOT_TOKEN', '<insert your token here>');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+
+
+function exec_curl_request($handle) {
+  $response = curl_exec($handle);
+
+  if ($response === false) {
+    $errno = curl_errno($handle);
+    $error = curl_error($handle);
+    error_log("Curl returned error $errno: $error\n");
+    curl_close($handle);
+    return false;
+  }
+
+  $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+  curl_close($handle);
+
+  if ($http_code >= 500) {
+    // do not wat to DDOS server if something goes wrong
+    sleep(10);
+    return false;
+  } else if ($http_code != 200) {
+    $response = json_decode($response, true);
+    error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+    if ($http_code == 401) {
+      throw new Exception('Invalid access token provided');
+    }
+    return false;
+  } else {
+    $response = json_decode($response, true);
+    if (isset($response['description'])) {
+      error_log("Request was successfull: {$response['description']}\n");
+    }
+    $response = $response['result'];
+  }
+
+  return $response;
+}
+
+function apiRequest($method, $parameters=null) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+  $url = API_URL.$method.'?'.http_build_query($parameters);
+
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+
+  return exec_curl_request($handle);
+}
+
+function apiRequestJson($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  $parameters["method"] = $method;
+
+  $handle = curl_init(API_URL);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));
+  curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
+  return exec_curl_request($handle);
+}
+
+function getUpdates($last_id = null){
+  $params = [];
+  if (!empty($last_id)){
+    $params = ['offset' => $last_id+1, 'limit' => 5];
+  }
+  //echo print_r($params, true);
+  return apiRequest('getUpdates', $params);
+}
+function printUpdates($result){
+  foreach($result as $obj){
+    echo $obj['message']['text'].PHP_EOL;
+    $last_id = $obj['update_id'];
+  }
+  return $last_id;
+}
+
+$last_id = null;
+while (true){
+  $result = getUpdates($last_id);
+  if (!empty($result)) {
+    $last_id = printUpdates($result);
+  }
+  
+  sleep(5);
+}
